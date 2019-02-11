0fec4095ece12f33de869873f29e92b6ea7632ea
chengjiawen <chengjiawen@glaroam2-144-151.wireless.gla.ac.uk>
2019-02-11 17:31:42 +0000

    database

--- tango_with_django_project/rango/tests.py
+++ tango_with_django_project/rango/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase
+from rango.models import Category
+class CategoryMethodTests(TestCase):
+  def test_ensure_views_are_positive(self):
+    cat= Category(name='test',views=-1, likes=0)
+    cat.save()
+    self.assertEqual((cat.views >= 0), True)
+
+  def test_slug_line_creation(self):
+    cat = cat('Random Category String')
+    cat.save()
+    self.assertEqual(cat.slug, 'random-category-string')
+
+
