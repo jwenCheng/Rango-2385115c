d556f8d745b28ab298b9c843a2ac85b983fd1dd8
chengjiawen <chengjiawen@glaroam2-144-151.wireless.gla.ac.uk>
2019-02-11 17:39:45 +0000

    chapter 9

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -133,3 +133,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
+LOGIN_URL = '/rango/login/'
--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -25,8 +25,11 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^rango/', include('rango.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^rango/about/', views.about, name='about'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^register/$',views.register,name='register'),
+    url(r'^login/$', views.user_login, name='login'),
+    url(r'^logout/$', views.user_logout, name='logout'),
+    url(r'^restricted/', views.restricted, name='restricted'),
     url(r'^add_category/$', views.add_category, name='add_category'),
-    url(r'^category/<category_name_- slug>/add_page/$', views.add_page, name='add_page'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$',views.show_category, name='show_category'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
