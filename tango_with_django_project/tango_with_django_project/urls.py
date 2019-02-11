4ca738b5b3e00a496949504304d8c3627ab8b24a
chengjiawen <chengjiawen@glaroam2-144-151.wireless.gla.ac.uk>
2019-02-11 17:37:54 +0000

    create base

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -20,6 +20,7 @@ from rango import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+app_name = 'rango'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^rango/', include('rango.urls')),
