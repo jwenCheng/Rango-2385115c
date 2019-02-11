f38305a6446eeb21580a57d9ed4bc2fc74d7899d
chengjiawen <chengjiawen@glaroam2-144-151.wireless.gla.ac.uk>
2019-02-11 17:35:50 +0000

    chapter7

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -25,4 +25,7 @@ urlpatterns = [
     url(r'^rango/', include('rango.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^rango/about/', views.about, name='about'),
+    url(r'^add_category/$', views.add_category, name='add_category'),
+    url(r'^category/<category_name_- slug>/add_page/$', views.add_page, name='add_page'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$',views.show_category, name='show_category'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
